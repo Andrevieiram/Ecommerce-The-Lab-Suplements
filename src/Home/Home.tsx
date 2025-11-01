763f7720f41c099347041d9191ddea9afa7c30eb
Andrevieiram <andrebrittu@gmail.com>
2025-10-31 22:58:31 -0300

    Adição da pagina de home e criação dos primeiros componentes

---
+++ src/Home/Home.tsx
@@ -0,0 +1,44 @@
+import './Home.css';
+const Home = () => {
+  return (
+    <div id="home-container">
+        <header id="header">
+            <button id="brand">
+                <img id="logo" src="images\WhatsApp_Image_2025-10-28_at_11.50.35-removebg-preview.png"></img>
+            </button>
+            <nav id="menu-nav">
+
+            </nav>
+            <button id="avatar">
+                <img id='picture' src='images\avatar.png'></img>
+            </button>
+        </header>
+
+        <section id="hero">
+
+        </section>
+
+        <section id="features">
+            
+        </section>
+
+        <section id="products">
+            
+        </section>
+
+        <section id="about">
+            
+        </section>
+
+        <footer id="footer">
+
+        </footer>
+    </div>
+  )
+}
+
+export default Home
+
+
+
+
