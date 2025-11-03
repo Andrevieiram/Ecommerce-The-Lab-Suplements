58629a57554d8c035b090a812fbd1b9366c9ec8e
Samuel Soares <samuelsoares.academic@gmail.com>
2025-11-02 22:32:53 -0300

    Criando arquivos bases da página de produtos

---
+++ src/Products/Products.tsx
@@ -0,0 +1,13 @@
+import './Products.css';
+
+const Products = () => {
+  return (
+    <div className = "products-container">
+        <div className="leftNav-container">
+            <h1>Teste</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Products
