2b8059e649b147ae85c9675a58690e1c6e9051ce
Gabriel Pinto Magalhaes <gabriel.p.magalhaes@icloud.com>
2025-11-03 19:06:21 -0300

    inserindo login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Home from './Home/Home'
 import Users from './Users/Users'
+import Login from './Login/Login';
 
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
+      <Route path="/Login" element={<Login />} />
       <Route path="/produto" element={<Home />} />
       <Route path="/users" element={<Users />} /> 
     </Routes>
---
+++ src/Login/Login.tsx
@@ -0,0 +1,26 @@
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div id="background-container">
+      <div id="login-container">
+        <h2>Sign in</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">User</label>
+            <input type="text" id="username" placeholder="Enter User" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter Password" />
+          </div>
+
+          <button type="submit">SIGN IN</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
