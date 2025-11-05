0719750988644bf17cde84d4cc8b188ddba9d72c
Samuel Soares <samuelsoares.academic@gmail.com>
2025-11-05 18:52:15 -0300

    Corrigindo rota

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path="/Login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/produto" element={<Home />} />
       <Route path="/users" element={<Users />} /> 
     </Routes>
